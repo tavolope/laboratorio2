f51d907801e7b0bcaf50f593a76f3599979f8d6a
gustavo <tavoadolfolop@gmail.com>
2023-06-15 10:03:24 -0600

    Se agrego la raiz

--- operaciones.py
+++ operaciones.py
@@ -1,3 +1,5 @@
+
+
 def suma (a,b):
     '''suma 2 valores'''
     resultado = a+b
@@ -7,3 +9,27 @@ def resta (a,b):
     '''resta 2 valores '''
     return  a-b
     
+def multiplicacion (a,b):
+    '''multiplica 2 valores '''
+    return  a*b
+
+def division (a,b):
+    '''divide 2 valores '''
+    return  a/b
+
+def exponenciacion (a,b):
+    '''eleva un numero'''
+    return  a**b
+
+def raices (a):
+    ''' raiz de 1 valor '''
+    import math
+    return math.sqrt (a)
+
+
+
+def run ():
+    pass
+
+if __name__== '_main_':
+    run()
