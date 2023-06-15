642f42b3a07608991d2c6f3a77b6776175b33c43
gustavo <tavoadolfolop@gmail.com>
2023-06-15 10:21:32 -0600

    Se agrego la suma de 5 digitos

--- operaciones.py
+++ operaciones.py
@@ -1,3 +1,5 @@
+
+
 def suma (a,b):
     '''suma 2 valores'''
     resultado = a+b
@@ -7,3 +9,28 @@ def resta (a,b):
     '''resta 2 valores '''
     return  a-b
     
+def multiplicacion (a,b):
+    '''multiplica 2 valores '''
+    return  a*b
+
+def division (a,b):
+    '''divide 2 valores '''
+    return  a/b
+
+def exponenciacion (a,b):
+    '''eleva un numero'''
+    return  a**b
+
+def raices (a):
+    ''' raiz de 1 valor '''
+    import math
+    return math.sqrt (a)
+
+def suma5 ():
+    suma = 0
+    for i in range (0,5):
+        h= int (input ("ingrese un numero "))
+        suma = suma + h 
+    print (suma)
+
+suma5()
